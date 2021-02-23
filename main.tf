430f10aa77cdbfdd40c6513430569441487becc9
mehulsharma2010 <mehul.sharma@opstree.com>
2021-02-23 16:00:05 +0530

    Adding images

--- main.tf
+++ main.tf
@@ -6,22 +6,20 @@ provider "google" {
   zone        = "us-central1-c"
 }
 
-# terraform {
-#   backend "local" {
-#     path = "/tmp/terraform.tfstate"
-#   }
-# }
+terraform {
+  backend "local" {
+    path = "/tmp/terraform.tfstate"
+  }
+}
 
 #VPC creation
 resource "google_compute_network" "vpc_network" {
-  #name = "${var.resource_name}-vpc"
-  name = "opstreee-vpc"
+  name = "${var.resource_name}-vpc"
 }
 
 #VM creation 
 resource "google_compute_instance" "vm_instance" {
- name = "opstreee-instance"
- # name         = "${var.resource_name}-instance"
+  name         = "${var.resource_name}-instance"
   machine_type = "f1-micro"
   boot_disk {
     initialize_params {
@@ -37,7 +35,7 @@ resource "google_compute_instance" "vm_instance" {
 
 #Firewall Creation
 resource "google_compute_firewall" "default_firewall" {
-  name    = "opstree-firewall"
+  name    = "${var.resource_name}-firewall"
   network = google_compute_network.vpc_network.name
 
   allow {
@@ -54,8 +52,7 @@ resource "google_compute_firewall" "default_firewall" {
 
 #Subnetwork creation
 resource "google_compute_subnetwork" "subnet-1" {
-  name = "opstreee-subnetwork"
-  #name          = "${var.resource_name}-subnetwork"
+  name          = "${var.resource_name}-subnetwork"
   ip_cidr_range = "10.2.0.0/16"
   region        = "us-central1"
   network       = google_compute_network.vpc_network.id
